4a7fef553351f6c253f7139276f94067287eb690
ookimaki <ookimaki@givinglog.com>
2018-07-26 00:53:29 +0900

    add basic front end with web3

--- contracts/SimpleStore.sol
+++ contracts/SimpleStore.sol
@@ -1,16 +1,13 @@
 pragma solidity ^0.4.23;
 
 contract SimpleStore {
-    uint256 value;
-
-    event NewValueSet(uint256 indexed _value, address _sender);
-
-    function set(uint256 _value) public {
+    string value;
+    
+    function set(string _value) public {
         value = _value;
-        emit NewValueSet(value, msg.sender);
     }
 
-    function get() public view returns (uint256, address) {
-        return (value, msg.sender);
+    function get() public view returns (string) {
+        return (value);
     }
 }
